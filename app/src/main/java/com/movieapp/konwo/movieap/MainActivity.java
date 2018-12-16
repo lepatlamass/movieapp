9e938c585c829e8b1af9f9afd103fdc77ba2a276
Farouk Sabiou <sfarouk298@gmail.com>
2018-12-16 19:51:07 +0100

    srcoll position bug fixed

--- app/src/main/java/com/movieapp/konwo/movieap/MainActivity.java
+++ app/src/main/java/com/movieapp/konwo/movieap/MainActivity.java
@@ -1,21 +1,18 @@
 package com.movieapp.konwo.movieap;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.os.Bundle;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -40,17 +37,19 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String LOG_TAG = MoviesAdapter.class.getName();
+
+    private static final String BUNDLE_RECYCLER_LAYOUT = "recycler_layout";
+    private static final String LIST_STATE = "list_state";
+
+    ProgressDialog progressDialog;
     private RecyclerView recyclerView;
     private MoviesAdapter adapter;
     private List<Movie> movieList;
-    ProgressDialog progressDialog;
     private SwipeRefreshLayout swipeContainer;
     private AppCompatActivity activity = MainActivity.this;
-    public static final String LOG_TAG = MoviesAdapter.class.getName();
 
-    private static String LIST_STATE = "list_state";
     private Parcelable savedRecyclerLayoutState;
-    private static final String BUNDLE_RECYCLER_LAYOUT = "recycler_layout";
     private ArrayList<Movie> moviesInstance = new ArrayList<>();
 
     private MovieDatabase movieDatabase;
@@ -65,38 +64,24 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             moviesInstance = savedInstanceState.getParcelableArrayList(LIST_STATE);
             savedRecyclerLayoutState = savedInstanceState.getParcelable(BUNDLE_RECYCLER_LAYOUT);
-            // get viewModel from ViewModelProviders class
-            viewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
-
-            movieDatabase = MovieDatabase.getDatabase(this);
-            executor = new MovieExec();
+            showData();
         } else {
             initViews();
         }
 
+        // get viewModel from ViewModelProviders class
+        viewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
+
+        movieDatabase = MovieDatabase.getDatabase(this);
+        executor = new MovieExec();
+
     }
 
-
-
-    public Activity getActivity() {
-        Context context = this;
-        while (context instanceof ContextWrapper){
-            if (context instanceof Activity) {
-                return (Activity) context;
-            }
-            context = ((ContextWrapper) context).getBaseContext();
-        }
-        return null;
-    }
-
-    private void initViews() {
-
+    private void showData() {
         recyclerView = findViewById(R.id.recycler_view);
 
-        movieList = new ArrayList<>();
-//        adapter = new MoviesAdapter(this, movieList);
         adapter = new MoviesAdapter(this, moviesInstance);
-        if (getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+        if (getApplicationContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
             recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         } else {
             recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
@@ -106,6 +91,23 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         restoreLayoutManagerPosition();
         adapter.notifyDataSetChanged();
+    }
+
+    private void initViews() {
+        recyclerView = findViewById(R.id.recycler_view);
+
+        movieList = new ArrayList<>();
+        adapter = new MoviesAdapter(this, movieList);
+        if (getApplicationContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        } else {
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        }
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        //restoreLayoutManagerPosition();
+        //adapter.notifyDataSetChanged();
 
         swipeContainer = findViewById(R.id.main_activity);
         swipeContainer.setColorSchemeColors(android.R.color.white);
@@ -120,8 +122,6 @@ public class MainActivity extends AppCompatActivity {
         checkSortOder();
 
     }
-
-
 
     private void restoreLayoutManagerPosition() {
         if (savedRecyclerLayoutState != null) {
@@ -149,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
         movieList = new ArrayList<>();
         adapter = new MoviesAdapter(this, movieList);
 
-        if (getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+        if (getApplicationContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         } else {
             recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
@@ -192,17 +192,6 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("Error", t.getMessage());
                     Toast.makeText(MainActivity.this, "Error Fetching Data!", Toast.LENGTH_SHORT).show();
                 }
-
-//                @Override
-//                public void onResponse(Call<MoviesResponse> call1, Response<MoviesResponse> responses) {
-//
-//                }
-//
-//                @Override
-//                public void onFailure(Call<MoviesResponse> call,  Throwable t) {
-//                    Log.d("Error", t.getMessage());
-//                    Toast.makeText(MainActivity.this, "Error Fetching Data!", Toast.LENGTH_SHORT).show();
-//                }
             });
         }catch (Exception e) {
             Log.d("Error", e.getMessage());
@@ -241,17 +230,6 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("Error", t.getMessage());
                     Toast.makeText(MainActivity.this, "Error Fetching Data!", Toast.LENGTH_SHORT).show();
                 }
-
-//                @Override
-//                public void onResponse(Call<MoviesResponse> call1, Response<MoviesResponse> responses) {
-//
-//                }
-//
-//                @Override
-//                public void onFailure(Call<MoviesResponse> call,  Throwable t) {
-//                    Log.d("Error", t.getMessage());
-//                    Toast.makeText(MainActivity.this, "Error Fetching Data!", Toast.LENGTH_SHORT).show();
-//                }
             });
         }catch (Exception e) {
             Log.d("Error", e.getMessage());
@@ -278,13 +256,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-//    @Override
-//    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-//        Log.d(LOG_TAG, "Preferences updated");
-//        checkSortOder();
-//    }
-
     private void checkSortOder() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         String sortOrder = preferences.getString(
@@ -308,9 +279,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-        if (movieList.isEmpty()){
-            checkSortOder();
-        }
     }
 
     // load favorites by observing changes from the database
